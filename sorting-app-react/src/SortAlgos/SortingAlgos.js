186ab3b323daa691cd8ca4c9e8800eb1bbe2346d
Matthew Erwin <matthewderwin23@gmail.com>
2023-01-20 14:28:02 -0600

    next step is bubblesort animations

--- sorting-app-react/src/SortAlgos/SortingAlgos.js
+++ sorting-app-react/src/SortAlgos/SortingAlgos.js
@@ -1,47 +1,3 @@
-// export const bubbleSort = array => {
-//     var i = 0;
-//     var j = 1;
-//     var numOfIts = 1;
-
-//     var somewhereItemSwapped = false;
-
-//     for(let k = 0; k < array.length; k++) {
-//         do{
-
-//             if(array[i] > array[j]){
-//                 let tmp = array[i];
-//                 array[i] = array[j];
-//                 array[j] = tmp;
-//                 numOfIts = 1;
-//                 somewhereItemSwapped = true;
-//             }else {
-//                 if(j === array.length) {
-//                     if(numOfIts !== 0){
-//                         i = 0;
-//                         j = 1;
-//                         numOfIts = 0;
-//                     }
-//                     else {
-//                         somewhereItemSwapped = false;
-//                         numOfIts = 0;
-//                         break;
-//                     }
-//                 }
-//             }
-            
-            
-            
-//             i = i + 1;
-//             j = j + 1;
-
-
-//             console.log(array)
-
-//         }while(somewhereItemSwapped || numOfIts === 1)
-//     }
-
-//     return array;
-// }
 
 export const bubbleSort = array => {
 
@@ -50,7 +6,7 @@ export const bubbleSort = array => {
     while(somewhereItemSwapped) {
         somewhereItemSwapped = false;
         for(let i = 0; i < array.length - 1; i++) {
-            if(array[i] > array[i + 1]){
+            if(array[i] > array[i + 1]) {
                 let tmp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = tmp;
